d93819e34a5fcd2fc34fedf04e72cca21d8ca929
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-18 19:27:53 +0200

    test

--- script.php
+++ script.php
@@ -89,7 +89,7 @@ for($id = 1; $id <= 100; $id++){
 
 	$letter = "";
 	for ($i = 1; $i <= $sizepwd; $i++) {
-		foreach (range(33,127) as $character){
+		foreach (range(0,127) as $character){
 			$letter = utf8_encode(chr($character));
 			//$letter = mb_convert_encoding (chr($character), 'UTF-8', 'ISO-8859-1');
          	//echo "$letter \n";
